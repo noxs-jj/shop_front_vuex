e0eba73c13803fe12b69c5a6fcd703688a18c8d1
Jean-Jacques MOIROUX <jjmoiroux@gmail.com>
2018-08-08 22:06:57 +0100

    git init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shop Front Vuex
+
+## Dependencies
+- Backend https://github.com/noxsnono/shop_backapi_rails
+
+## Prerequies
+- node 10.8.0
+
+## How to start
+  npm install -g yarn
+  yarn run dev
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    basket: []
+  },
+  getters: {
+    basketProducts: state => {
+      return state.basket.length
+    }
+  },
+  mutations: {},
+  actions: {}
+})
